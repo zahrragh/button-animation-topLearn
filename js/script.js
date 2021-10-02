82d4beaeb7acb420c7fea29a4443eabbcf8b7cb8
zahra <zahrraghh@gmail.com>
2021-10-02 14:13:07 +0330

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const button = document.querySelectorAll('button')
+
+button.forEach(btn => {
+    btn.addEventListener('click', function (e) {
+        let x = e.clientX - e.target.offsetLeft;
+        let y = e.clientY - e.target.offsetTop;
+        let span = document.createElement('span');
+        span.style.left = x + 'px';
+        span.style.top = y + 'px';
+        this.appendChild(span);
+
+        setTimeout(()=>{
+            span.remove();
+        }, 1000)
+    })
+});
